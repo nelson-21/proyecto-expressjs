712dd595e5b858e6027af8111c525e0d2ec3b697
Nelson <nelsonjosuemf15@gmail.com>
2026-05-14 04:49:00 -0600

    CRUD post and put

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ require('dotenv').config();
  
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const fs = require('fs');
+const path = require('path');
+const userFilePath = path.join(__dirname, 'users.json');
+
  
 const app = express();
  
@@ -59,7 +64,58 @@ app.post('/api/data', (req, res) =>{
         data
     });
 });
- 
+
+app.get('/users', (req, res) =>{
+    fs.readFile(userFilePath, 'utf-8', (err, data) =>{
+        if(err){
+            return res.status(500).json({ error: 'Error con conexión de datos.'});
+        }
+        const users = JSON.parse(data);
+        res.json(users);
+    });
+});
+
+app.post('/users', (req, res) =>{
+    const newUser = req.body;
+    fs.readFile(userFilePath, 'utf-8', (err, data) =>{
+        if(err){
+            return res.status(500).json({ error: 'Error con conexión de datos.'});
+        }
+        const users = JSON.parse(data);
+        users.push(newUser);
+        fs.writeFile(userFilePath, JSON.stringify(users, null, 2), (err) =>{
+            if (err){
+                return res.status(500).json({ error: 'Error al guardar el usuario.'});
+            }
+            res.status(201).json(newUser);
+        });
+    });
+});
+
+
+app.put('/users/:id', (req, res) =>{
+    const userId = parseInt(req.params.id, 10)
+    const updateUser = req.body;
+
+    fs.readFile(userFilePath, 'utf-8', (err, data) =>{
+        if (err){
+            return res.status(500).json({ error: 'Error con conexión de datos.'});
+        }
+        let users = JSON.parse(data);
+        users = users.map(user =>
+            user.id === userId ? {...user, ...updateUser} : user
+        );
+        fs.writeFile(userFilePath, JSON.stringify(users, null, 2), (err) =>{
+            if (err){
+                return res
+                .status(500)
+                .json({ error: 'Error al actualizar el usuario' });
+            }
+            res.json(updateUser);
+        })
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor: http://localhost:${PORT}`);
 });
